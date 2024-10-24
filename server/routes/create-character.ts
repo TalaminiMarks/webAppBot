ac6c0ec1262cf6e79b6bcb9c2087228c1408bdab
TalaminiMarks <talamini2014@gmail.com>
2024-10-24 15:28:54 -0300

    add creating record on character table for test

--- server/routes/create-character.ts
+++ server/routes/create-character.ts
@@ -1,7 +1,24 @@
 import { FastifyInstance } from "fastify";
+import z from "zod";
+
+import { prisma } from "../utils/prisma";
 
 export default async function route (fastify: FastifyInstance){
-    fastify.post("/personagem/criar", (req, res)=>{
+    const schema = z.object({
+        id: z.number(),
+        name: z.string()
+    });
+
+    fastify.post("/personagem/criar", async (req, res)=>{
+        const data = schema.parse(req.body);
         
+        await prisma.character.create({
+            data: {
+                id: data.id,
+                name: data.name
+            }
+        })
+
+        res.send("Deu certo")
     });
 }
