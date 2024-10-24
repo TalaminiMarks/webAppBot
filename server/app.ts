e200df8d19589148d782f2eed2520bb3536ca5d8
TalaminiMarks <talamini2014@gmail.com>
2024-10-24 14:32:54 -0300

    use pino-pretty for log info

--- server/app.ts
+++ server/app.ts
@@ -3,13 +3,22 @@ import fastify from "fastify";
 import character from "./routes/character";
 import create_character from "./routes/create-character";
 
-const app = fastify();
+const app = fastify({
+    logger: {
+        level: "info",
+        transport: {
+            target: "pino-pretty"
+        }
+    }
+});
 
 app.register(character);
 app.register(create_character);
 
 app.listen({
     port: 3333
-}).then(()=>{
-    console.log("Server on http://localhost:3333");
+}, (err)=>{
+    if (err){
+        app.log.error(err);
+    }
 });
