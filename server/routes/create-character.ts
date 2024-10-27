02cb8f1e7cec794cc354a439559c77b677fd506e
TalaminiMarks <talamini2014@gmail.com>
2024-10-27 20:12:15 -0300

    add character class and initial item data to zod schema

--- server/routes/create-character.ts
+++ server/routes/create-character.ts
@@ -8,20 +8,23 @@ export default async function route (fastify: FastifyInstance){
     // Cria um zod schema para validação dos dados que serão recebidos
     const schema = z.object({
         id: z.string(),
-        name: z.string()
+        name: z.string(),
+        role: z.string(),
+        initial: z.string()
     });
     // Declaração da rota
     fastify.post("/personagem/criar", async (req, res)=>{
         // Recupera as info que foram passados na requisição e valida com o schema
         const data = schema.parse(req.body);
-        // Cria um campo no DB
-        await prisma.character.create({
-            data: {
-                id: data.id,
-                name: data.name
-            }
-        })
+        // Cria um registro no DB
+        // await prisma.character.create({
+        //     data: {
+        //         id: data.id,
+        //         name: data.name
+        //     }
+        // })
         // Retorna uma mensagem na requisição
+
         res.send(JSON.stringify({ message: 'Deu certo' }))
     });
 }
