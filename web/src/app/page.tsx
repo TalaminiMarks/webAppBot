2883e52a478bbd825b04afa7083d3af93d4d3b1d
TalaminiMarks <talamini2014@gmail.com>
2024-11-04 09:48:07 -0300

    add data from db and some template for later

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,5 +1,16 @@
-export default function Home() {
-  return (
-    <div>cu</div>
-  );
+import { axiosInstance } from "@/utils/utils";
+
+export default async function Home() {
+    const res = await axiosInstance.get("/personagens");
+    console.log(res.data)
+    return (
+        <main className="w-full flex justify-center items-center">
+            <section className="w-1/2 p-5 flex justify-center items-center flex-col bg-slate-500">
+                <header className="w-1/2 bg-red-200 py-4 text-center">Personagens</header>
+                <div className="py-4 text-center">
+                    <p>asd</p>
+                </div>
+            </section>
+        </main>
+    );
 }
