0cd86eccf7d6de88faaf8c3146a363a481e8969e
TalaminiMarks <talamini2014@gmail.com>
2024-11-04 08:13:31 -0300

    add db data to '/personagens' route

--- server/routes/character.ts
+++ server/routes/character.ts
@@ -1,8 +1,12 @@
 import { FastifyInstance } from "fastify";
 
+import { prisma } from "../utils/prisma";
+
 // Rota para recuperar todos os personagens do DB
 export default async function route(fastify: FastifyInstance){
-    fastify.get("/personagem", (req, res)=>{
-        res.send({ rota: "personagens" });
+    fastify.get("/personagens", async (req, res)=>{
+        const data = await prisma.character.findMany();
+
+        res.send(data);
     });
 }
