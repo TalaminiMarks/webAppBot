53773a13db63df1a4823e7762b9367acd846aa2a
TalaminiMarks <talamini2014@gmail.com>
2024-11-05 15:25:04 -0300

    change '/personagens' route to query db dynamically by id

--- server/routes/character.ts
+++ server/routes/character.ts
@@ -1,11 +1,21 @@
 import { FastifyInstance } from "fastify";
+import z from 'zod';
 
 import { prisma } from "../utils/prisma";
 
 // Rota para recuperar todos os personagens do DB
 export default async function route(fastify: FastifyInstance){
-    fastify.get("/personagens", async (req, res)=>{
-        const data = await prisma.character.findMany();
+    fastify.get("/personagens/:id", async (req, res)=>{
+        const schema = z.object({
+            id: z.string()
+        })
+
+        const params = schema.parse(req.params)
+        const data = await prisma.character.findUniqueOrThrow({
+            where: {
+                id: params.id
+            }
+        });
 
         res.send(data);
     });
