1ac7ab88e05b511053a7a13d5d41581f09bcb04d
TalaminiMarks <talamini2014@gmail.com>
2024-11-06 15:15:51 -0300

    add Header component to main page

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,39 +1,9 @@
+import Header from "@/components/Header";
+
 export default async function Home() {
     return (
         <main className="w-full bg-slate-300">
-            <header className="py-6 flex justify-between items-center">
-                <section className="w-1/3 bg-red-300">
-                    <span>algum nome</span>
-                    <p>Nome do personagem</p>
-                </section>
-
-                <section className="w-2/3 grid grid-cols-3 justify-center items-center bg-blue-200">
-                    <div>
-                        <span>..;</span>
-                        <p>Classe e nivel</p>
-                    </div>
-                    <div>
-                        <span>..;</span>
-                        <p>Antecedente</p>
-                    </div>
-                    <div>
-                        <span>..;</span>
-                        <p>Nome do jogador</p>
-                    </div>
-                    <div>
-                        <span>..;</span>
-                        <p>Raça</p>
-                    </div>
-                    <div>
-                        <span>..;</span>
-                        <p>Tendência</p>
-                    </div>
-                    <div>
-                        <span>..;</span>
-                        <p>Pontos de Experiencia</p>
-                    </div>
-                </section>
-            </header>
+            <Header name="Marico" role="Ladrão" userName="Maricock"/>
 
             <section>
                 <div className="bg-yellow-200 w-1/3">
