5408adccff674458660f0f4953e2839664ed7443
TalaminiMarks <talamini2014@gmail.com>
2024-11-09 09:39:49 -0300

    add column2 component

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,17 +1,20 @@
 import Column1 from "@/components/Column1";
+import Column2 from "@/components/Column2";
 import Header from "@/components/Header";
 
 export default async function Home() {
     return (
         <main className="w-full bg-slate-300">
             <Header name="Marico" role="Ladrão" userName="Maricock"/>
-
-            <section className="w-1/3 bg-blue-300">
-                <Column1 />
-
-            </section>
-            <section className="w-1/3"></section>
-            <section className="w-1/3"></section>
+            <div className="grid grid-cols-3">
+                <section className="w-full bg-blue-300">
+                    <Column1 />
+                </section>
+                <section className="w-full bg-blue-400">
+                    <Column2 />
+                </section>
+                <section className="w-full"></section>
+            </div>
         </main>
     )
 }
