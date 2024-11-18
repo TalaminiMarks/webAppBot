f72712016ba7e647ed6bc519d532d58703342ac4
TalaminiMarks <talamini2014@gmail.com>
2024-11-18 08:23:58 -0300

    change span to input

--- web/src/components/column1/BoxWithText.tsx
+++ web/src/components/column1/BoxWithText.tsx
@@ -1,21 +1,38 @@
 import { ReactNode } from "react";
 
 interface FieldProps {
-    attribute: string,
+    attribute: string
+    attributeChecked?: boolean
     value?: number
 }
 
-export function Field({attribute, value}: FieldProps){
+export function Field({attribute, attributeChecked, value}: FieldProps){
     return (
         <div className="flex items-center gap-2">
-            <input type="checkbox" name={attribute} id={attribute} />
-            <span  className="px-4 bg-purple-300 text-center">{value ? value : 0}</span>
-            <span className="capitalize text-sm">{attribute}</span>
+            <input 
+                type="checkbox" 
+                name={attribute + "check"} 
+                id={attribute + "check"} 
+                defaultChecked={attributeChecked ? attributeChecked : false}
+            />
+            <input 
+                type="text" 
+                name={attribute}
+                id={attribute + "proef"} 
+                className="w-8 bg-purple-300 text-center" 
+                defaultValue={value ? value : 0} 
+            />
+            <label htmlFor={attribute + "proef"} className="capitalize text-sm">{attribute}</label>
         </div>
     )
 }
 
-export function BoxWithText({children, description}: {children: ReactNode, description: string}){
+interface BoxWithTextProps {
+    children: ReactNode
+    description: string
+}
+
+export function BoxWithText({children, description}: BoxWithTextProps){
     return(
         <div className="w-full flex flex-col px-4 py-2 gap-2 bg-red-300">
             {children}
