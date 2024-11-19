ea6b529a47472f99be821ad82f556b8647177674
TalaminiMarks <talamini2014@gmail.com>
2024-11-19 17:14:36 -0300

    add script to remove all record from db

--- server/src/utils/deleteRecords.ts
+++ server/src/utils/deleteRecords.ts
@@ -1,5 +1,10 @@
 import { prisma } from "./prisma";
 
 Promise.all([
-    prisma.character.deleteMany()
+    prisma.character.deleteMany(),
+    prisma.attributes.deleteMany(),
+    prisma.expertise.deleteMany(),
+    prisma.items.deleteMany(),
 ])
+.then(console.log)
+.catch(console.error)
