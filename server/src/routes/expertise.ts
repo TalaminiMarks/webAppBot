d8c4cccc62643bc06b159d5642972478094a89ab
TalaminiMarks <talamini2014@gmail.com>
2024-11-23 17:05:29 -0300

    add pericias route

---
+++ server/src/routes/expertise.ts
@@ -0,0 +1,16 @@
+import { FastifyInstance } from "fastify";
+import { prisma } from "../utils/prisma";
+
+export default async function expertise(fastify: FastifyInstance) {
+    fastify.get('/pericias', async (req, res)=>{
+        const expertise = await prisma.expertise.findMany({
+            select: {
+                id: true,
+                name: true
+            },
+            orderBy: {
+                id: 'asc'
+            }
+        })
+    })
+}
