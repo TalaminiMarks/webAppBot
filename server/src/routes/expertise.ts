e7ff55138a1342025f70aeea07e8b4b65f6469a1
TalaminiMarks <talamini2014@gmail.com>
2024-11-23 18:53:26 -0300

    change route to get the expertise value

--- server/src/routes/expertise.ts
+++ server/src/routes/expertise.ts
@@ -6,12 +6,20 @@ export default async function expertise(fastify: FastifyInstance) {
         const expertise = await prisma.expertise.findMany({
             select: {
                 id: true,
-                name: true
+                name: true,
+                CharacterExpertise: {
+                    select: {
+                        value: true,
+                        characterId: true,
+                        expertiseId: true
+                    }
+                }
             },
             orderBy: {
                 id: 'asc'
             }
         })
+        
         res.send(expertise)
     })
 }
