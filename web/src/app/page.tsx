b102bc823ec01b06d3620c4d25ee2d0fde65b44e
TalaminiMarks <talamini2014@gmail.com>
2024-11-25 20:30:47 -0300

    add padding y

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -2,12 +2,43 @@ import Column1 from "@/components/Column1";
 import Column2 from "@/components/Column2";
 import Column3 from "@/components/Column3";
 import Header from "@/components/Header";
+import { axiosInstance } from "@/utils/utils";
+
+interface CharacterInfo {
+    name: string,
+    role: string,
+    health: number,
+    age: number,
+    race: string,
+    languages: string,
+    affiliation: string,
+    previous: string,
+    defect: string,
+    ideas: string,
+    personality: string,
+    history: string,
+    xp: number,
+    gold: number
+}
 
 export default async function Home() {
+    const id = 123123;
+    const character = await axiosInstance.get(`/personagens/${id}`)
+
+    const data: CharacterInfo = character.data;
+
     return (
         <main className="w-full bg-slate-300">
-            <Header name="Marico" role="Ladrão" userName="Maricock"/>
-            <div className="grid grid-cols-3">
+            <Header 
+                name={data.name} 
+                role={data.role} 
+                previous={data.previous}
+                race={data.race}
+                xp={data.xp}
+                tendency="algo"
+                userName="Maricock"
+            />
+            <div className="grid grid-cols-3 py-4">
                 <section className="w-full bg-blue-300">
                     <Column1 />
                 </section>
