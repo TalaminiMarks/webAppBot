6aba652ed2981aa718c0ea9c3823acf0cc356da0
TalaminiMarks <talamini2014@gmail.com>
2024-11-25 20:34:21 -0300

    fix value properties on Fields components

--- web/src/components/Header.tsx
+++ web/src/components/Header.tsx
@@ -1,20 +1,23 @@
 interface characterInfo {
     name: string,
     role: string,
-    userName: string,
-    exp?: number
+    race: string,
+    previous: string,
+    xp: number,
+    tendency: string,
+    userName: string
 }
 
 function Field({value, description}: {value?: string | number, description: string}){
     return (
         <div>
-            <span className="text-xl font-bold">{value ? value : "algo"}</span>
+            <span className="text-xl font-bold">{value}</span>
             <p className="uppercase text-header-field">{description}</p>
         </div>
     )
 }
 
-export default function Header({name, role, userName, exp}: characterInfo){
+export default function Header({name, role, previous, race, userName, tendency, xp}: characterInfo){
     return (
         <header className="py-6 flex justify-between items-center">
             <section className="w-1/3 bg-red-300">
@@ -23,11 +26,11 @@ export default function Header({name, role, userName, exp}: characterInfo){
         
             <section className="w-2/3 grid grid-cols-3 justify-center items-center bg-blue-200">
                 <Field description="classe e nível" value={role}/>
-                <Field description="Antecedente"/>
+                <Field description="Antecedente" value={previous}/>
                 <Field description="Nome do jogador" value={userName}/>
-                <Field description="Raça"/>
-                <Field description="Tendência"/>
-                <Field description="Pontos de Experiencia" value={exp ? exp : 0}/>
+                <Field description="Raça" value={race}/>
+                <Field description="Tendência" value={tendency}/>
+                <Field description="Pontos de Experiencia" value={xp}/>
             </section>
         </header>
     )
