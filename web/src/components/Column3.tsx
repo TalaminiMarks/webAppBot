178b74aac3fa88d4c401b73564497ce817fda4f6
TalaminiMarks <talamini2014@gmail.com>
2024-11-26 09:25:20 -0300

    add values to components

--- web/src/components/Column3.tsx
+++ web/src/components/Column3.tsx
@@ -1,13 +1,21 @@
 import TextAreaField from "./column3/TextAreaField";
 
-export default function Column3(){
+interface CharacterInfo{
+    personality: string,
+    ideas: string,
+    affiliation: string,
+    defect: string,
+    particulars: string
+}
+
+export default function Column3({affiliation, defect, ideas, particulars, personality}: CharacterInfo){
     return (
         <div className="w-full flex flex-col items-center justify-center px-8 gap-2">
-            <TextAreaField description="traços de personalidade" id="personality" />
-            <TextAreaField description="ideais" id="ideas" />
-            <TextAreaField description="ligaçoes" id="contacts" />
-            <TextAreaField description="defeitos" id="defect" />
-            <TextAreaField description="características e habilidades" id="particulars" rows={25}/>
+            <TextAreaField description="traços de personalidade" id="personality" value={personality}/>
+            <TextAreaField description="ideais" id="ideas" value={ideas}/>
+            <TextAreaField description="ligaçoes" id="affiliation" value={affiliation}/>
+            <TextAreaField description="defeitos" id="defect" value={defect}/>
+            <TextAreaField description="características e habilidades" id="particulars" rows={25} value={particulars}/>
         </div>
     )
 }
