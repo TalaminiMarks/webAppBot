23b858a7967e832df332f73d00abc425af9aa1d9
TalaminiMarks <talamini2014@gmail.com>
2024-11-26 09:32:50 -0300

    add totalHealth and currentHealth to component

--- web/src/components/Column2.tsx
+++ web/src/components/Column2.tsx
@@ -2,7 +2,12 @@ import { Health, HealthDice, TemporaryHealth, Death, BasicInfo } from "./column2
 import Hits from "./column2/Hits";
 import Equipament from "./column2/Equipament";
 
-export default function Column2(){
+interface CharacterInfo{
+    totalHealth: number,
+    currentHealth?: number
+}
+
+export default function Column2({totalHealth, currentHealth}: CharacterInfo){
     return(
         <div className="w-full flex flex-col justify-center items-center px-8 gap-4">
             <div className="w-full flex justify-between items-center">
@@ -10,7 +15,7 @@ export default function Column2(){
                 <BasicInfo description="iniciativa"/>
                 <BasicInfo description="desloc."/>
             </div>
-            <Health totalHealth={0}/>
+            <Health totalHealth={totalHealth} currentHealth={currentHealth ? currentHealth : totalHealth}/>
             <TemporaryHealth />
             <div className="w-full flex justify-center items-center gap-4">
                 <HealthDice />
