433930e1e03329f1744da0ab0b35c2ab37be8c9d
TalaminiMarks <talamini2014@gmail.com>
2024-12-06 18:39:48 -0300

    add equipaments/id route to search each equipament on character

--- server/src/routes/equipament.ts
+++ server/src/routes/equipament.ts
@@ -1,9 +1,25 @@
 import { FastifyInstance } from "fastify";
 import { prisma } from "../utils/prisma";
+import z from "zod";
 
 export default async function equipament(fastify: FastifyInstance){
-    fastify.get("/equipamentos", async (req, res)=>{
-        const data = await prisma.items.findMany();
+    fastify.get("/equipamentos/:id", async (req, res)=>{
+        const schema = z.object({
+            id: z.string().transform(val => Number(val))
+        })
+
+        const { id } = schema.parse(req.params);
+
+        const data = await prisma.items.findUniqueOrThrow({
+            where: {
+                id: id
+            },
+            select: {
+                id: true,
+                name: true,
+                description: true
+            }
+        });
 
         res.send(data);
     })
