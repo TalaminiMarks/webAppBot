21fad54cf78a711897672f25e5aba431fed8b1a5
TalaminiMarks <talamini2014@gmail.com>
2024-12-07 14:43:37 -0300

    add separated file for types and interface to column2

---
+++ web/src/components/column2/types.ts
@@ -0,0 +1,23 @@
+export interface DataItemFieldProps {
+    id: number,
+    name: string,
+    description: string
+}
+
+export interface EquipamentProps{
+    gold: number
+    CharacterItens: [{
+        id: string,
+        itemsId: number
+    }]
+}
+
+export interface ItemFieldProps {
+    name: string,
+    description: string
+}
+
+export interface FieldProps {
+    name: string
+    value?: number
+}
