5e4293722dab93e2e01052c036c84ad9516a1c7d
TalaminiMarks <talamini2014@gmail.com>
2024-12-11 10:06:02 -0300

    update deleteRecords script

--- server/src/utils/deleteRecords.ts
+++ server/src/utils/deleteRecords.ts
@@ -3,11 +3,21 @@ import { prisma } from "./prisma";
 Promise.all([
     prisma.characterAttributes.deleteMany(),
     prisma.characterExpertise.deleteMany(),
+    prisma.characterSkills.deleteMany(),
+    prisma.characterItens.deleteMany(),
+    prisma.characterSpells.deleteMany(),
+    prisma.characterDeath.deleteMany()
+])
+.then(console.log)
+.catch(console.error)
+
+Promise.all([
     prisma.character.deleteMany(),
     prisma.attributes.deleteMany(),
     prisma.expertise.deleteMany(),
     prisma.items.deleteMany(),
-    prisma.characterItens.deleteMany()
+    prisma.skills.deleteMany(),
+    prisma.spells.deleteMany(),
 ])
 .then(console.log)
 .catch(console.error)
