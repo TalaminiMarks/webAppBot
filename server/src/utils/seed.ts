e280124a8ec1f509e9883b4ef20404245a874355
TalaminiMarks <talamini2014@gmail.com>
2024-12-12 16:37:55 -0300

    update seed script

--- server/src/utils/seed.ts
+++ server/src/utils/seed.ts
@@ -38,32 +38,61 @@ async function expertise(){
     })
 }
 
+async function items(){
+    await prisma.items.createMany({
+        data: [
+            {name: 'espada larga', description: 'Usada como arma'},
+            {name: 'espada longa', description: 'Usada como arma'}
+        ]
+    })
+}
+
+async function skills(){
+    await prisma.skills.createMany({
+        data: [
+            {name: "Estilos de combate", description: "uma skill"},
+            {name: "retomar folego", description: "uma skill"}
+        ]
+    })
+}
+
+async function spells(){
+    await prisma.spells.createMany({
+        data: [
+            {name: "missel arcano", description: "uma spell"},
+            {name: "armadura arcana", description: "uma spell"}
+        ]
+    })
+}
+
 async function character(){
     const Attributes = await prisma.attributes.findMany();
+    const Expertise = await prisma.expertise.findMany();
+    const Items = await prisma.items.findMany();
+    const Skills = await prisma.skills.findMany();
+    const Spells = await prisma.spells.findMany();
 
     const Character = await prisma.character.create({
         data: {
             id: "123123",
-            name: "Mariko",
+            name: "Tester",
             age: "20",
             race: "humano",
-            role: "Pyromante",
-            CharacterAttributes: {
-                createMany: {
-                    data: [
-                        {attributesId: Attributes[0].id, value: 5},                 
-                        {attributesId: Attributes[1].id},                 
-                        {attributesId: Attributes[2].id, value: 3},                 
-                        {attributesId: Attributes[3].id},                 
-                        {attributesId: Attributes[4].id},                 
-                        {attributesId: Attributes[5].id, value: 7}                 
-                    ]
-                }
-            }
+            role: "Guerreiro",
         }
     })
 
-    const Expertise = await prisma.expertise.findMany();
+    await prisma.characterAttributes.createMany({
+        data: [
+            {characterId: Character.id, attributesId: Attributes[0].id, value: 6},
+            {characterId: Character.id, attributesId: Attributes[1].id, value: 9},
+            {characterId: Character.id, attributesId: Attributes[2].id},
+            {characterId: Character.id, attributesId: Attributes[3].id},
+            {characterId: Character.id, attributesId: Attributes[4].id, value: 10},
+            {characterId: Character.id, attributesId: Attributes[5].id}
+        ]
+    })
+    
 
     await prisma.characterExpertise.createMany({
         data: [
@@ -82,34 +111,40 @@ async function character(){
             {characterId: Character.id, expertiseId: Expertise[12].id, value: 3},
             {characterId: Character.id, expertiseId: Expertise[13].id},
             {characterId: Character.id, expertiseId: Expertise[14].id},
-            {characterId: Character.id, expertiseId: Expertise[15].id},
+            {characterId: Character.id, expertiseId: Expertise[15].id, value: 98},
             {characterId: Character.id, expertiseId: Expertise[16].id},
             {characterId: Character.id, expertiseId: Expertise[17].id}
         ]
     })
 
-    const Items = await prisma.items.findMany();
-
     await prisma.characterItens.create({
         data: { 
             characterId: Character.id,
-            itemsId: Items[0].id
+            itemsId: Items[0].id,
+            value: "1d6"
+        }
+    })
+
+    await prisma.characterSkills.create({
+        data: {
+            characterId: Character.id,
+            skillsId: Skills[0].id,
+            value: "1d6"
+        }
+    })
+
+    await prisma.characterSpells.create({
+        data: {
+            characterId: Character.id,
+            spellsId: Spells[0].id,
+            value: "1d6"
         }
     })
     
 }
 
-async function items(){
-    await prisma.items.createMany({
-        data: [
-            {name: 'espada larga', description: 'Usada como arma'},
-            {name: 'espada longa', description: 'Usada como arma'}
-        ]
-    })
-}
-
 async function main(){
-    await Promise.all([attributes(), expertise(), items()])
+    await Promise.all([attributes(), expertise(), items(), skills(), spells()])
     await character()
 }
 
