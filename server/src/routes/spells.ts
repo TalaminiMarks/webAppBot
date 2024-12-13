8579cd620b3057f2e3f6da2426c59cf0777aaa03
TalaminiMarks <talamini2014@gmail.com>
2024-12-13 10:39:59 -0300

    add spells route

---
+++ server/src/routes/spells.ts
@@ -0,0 +1,26 @@
+import { FastifyInstance } from "fastify";
+import { z } from "zod";
+import { prisma } from "../utils/prisma";
+
+export default function spells(fastify: FastifyInstance){
+    fastify.get("/magias/:id", async (req, res)=>{
+        const schema = z.object({
+            id: z.string().transform(val => Number(val))
+        })
+
+        const { id } = schema.parse(req.params);
+
+        const data = await prisma.spells.findUniqueOrThrow({
+            where: {
+                id: id
+            },
+            select: {
+                id: true,
+                name: true,
+                description: true 
+            }
+        })
+
+        res.send(data)
+    })
+}
