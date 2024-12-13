17ed458b09bba02cd0d44f22dcfaa8a1876745f5
TalaminiMarks <talamini2014@gmail.com>
2024-12-13 10:18:04 -0300

    update deleteRecords script

--- server/src/utils/deleteRecords.ts
+++ server/src/utils/deleteRecords.ts
@@ -1,23 +1,27 @@
 import { prisma } from "./prisma";
 
-Promise.all([
-    prisma.characterAttributes.deleteMany(),
-    prisma.characterExpertise.deleteMany(),
-    prisma.characterSkills.deleteMany(),
-    prisma.characterItens.deleteMany(),
-    prisma.characterSpells.deleteMany(),
-    prisma.characterDeath.deleteMany()
-])
-.then(console.log)
-.catch(console.error)
+async function deleteRecords() {
+    await Promise.all([
+        prisma.characterAttributes.deleteMany(),
+        prisma.characterExpertise.deleteMany(),
+        prisma.characterSkills.deleteMany(),
+        prisma.characterItens.deleteMany(),
+        prisma.characterSpells.deleteMany(),
+        prisma.characterDeath.deleteMany()
+    ])
+    .then(console.log)
+    .catch(console.error)
+    
+    await Promise.all([
+        prisma.character.deleteMany(),
+        prisma.attributes.deleteMany(),
+        prisma.expertise.deleteMany(),
+        prisma.items.deleteMany(),
+        prisma.skills.deleteMany(),
+        prisma.spells.deleteMany(),
+    ])
+    .then(console.log)
+    .catch(console.error)
+}
 
-Promise.all([
-    prisma.character.deleteMany(),
-    prisma.attributes.deleteMany(),
-    prisma.expertise.deleteMany(),
-    prisma.items.deleteMany(),
-    prisma.skills.deleteMany(),
-    prisma.spells.deleteMany(),
-])
-.then(console.log)
-.catch(console.error)
+deleteRecords()
