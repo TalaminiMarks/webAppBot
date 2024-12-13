804093fbf3a5c1d47fa6259ff2a6a34f398f846d
TalaminiMarks <talamini2014@gmail.com>
2024-12-13 10:34:43 -0300

    add skills route

---
+++ server/src/routes/skills.ts
@@ -0,0 +1,26 @@
+import { FastifyInstance } from "fastify";
+import z from "zod";
+import { prisma } from "../utils/prisma";
+
+export default function skills(fastify: FastifyInstance){
+    fastify.get("/habilidades/:id", async (req, res)=>{
+        const schema = z.object({
+            id: z.string().transform(val => Number(val))
+        })
+
+        const { id } = schema.parse(req.params);
+
+        const data = await prisma.skills.findUniqueOrThrow({
+            where: {
+                id: id
+            },
+            select: {
+                id: true,
+                name: true,
+                description: true
+            }
+        })
+
+        res.send(data)
+    })
+}
