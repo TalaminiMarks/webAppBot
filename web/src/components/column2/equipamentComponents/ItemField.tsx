36b76430d9bcab1ae49991933ffb0e88f0760aab
TalaminiMarks <talamini2014@gmail.com>
2025-01-08 09:48:44 -0300

    add function to delete item

--- web/src/components/column2/equipamentComponents/ItemField.tsx
+++ web/src/components/column2/equipamentComponents/ItemField.tsx
@@ -2,10 +2,11 @@
 
 import { useRef } from "react";
 import { MinusIcon } from "@heroicons/react/24/solid";
+import { axiosInstance } from "@/utils/utils";
 
 import { ItemFieldProps } from "../types";
 
-export default function ItemField({name, description}: ItemFieldProps){
+export default function ItemField({id, name, description}: ItemFieldProps){
     const ref = useRef<HTMLDivElement>(null);
 
     function handleMouseEnter(){
@@ -19,10 +20,14 @@ export default function ItemField({name, description}: ItemFieldProps){
             ref.current.style.display = "none";
         }
     }
+
+    async function deleteRecord(){
+        await axiosInstance.delete(`deletar-item/${id}`)
+    }
     return (
         <div className="w-full flex justify-between items-center p-2 relative">
             <span onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>{name}</span>
-            <button className="w-6 h-6 rounded bg-black text-white hover:bg-white hover:text-black transition">
+            <button className="w-6 h-6 rounded bg-black text-white hover:bg-white hover:text-black transition" onClick={deleteRecord}>
                 <MinusIcon />
             </button>
             <div ref={ref} className="hidden absolute top-10 left-0 w-full rounded-xl p-2 bg-yellow-200 z-10">
