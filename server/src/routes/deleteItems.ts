1890f31baf4787610fbe2b2a58f7d073668429bb
TalaminiMarks <talamini2014@gmail.com>
2025-01-08 09:44:11 -0300

    add route to delete items on character

---
+++ server/src/routes/deleteItems.ts
@@ -0,0 +1,21 @@
+import { FastifyInstance } from "fastify";
+import { prisma } from "../utils/prisma";
+import z from "zod"
+
+export default function deleteItems(fastify: FastifyInstance){
+    fastify.delete("/deletar-item/:id", async (req, res)=>{
+        const schema = z.object({
+            id: z.string()
+        })
+
+        const params = schema.parse(req.params);
+
+        await prisma.characterItens.delete({
+            where: {
+                id: params.id
+            }
+        })
+
+        res.send("Item deletado com sucesso")
+    })
+}
