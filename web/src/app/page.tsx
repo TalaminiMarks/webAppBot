a65ce76e2d1731fdbd415a5abfc5d1b878d41774
TalaminiMarks <talamini2014@gmail.com>
2025-01-13 15:02:32 -0300

    move CharacterInfo interface to separated file

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -2,60 +2,8 @@ import Column1 from "@/components/Column1";
 import Column2 from "@/components/Column2";
 import Column3 from "@/components/Column3";
 import Header from "@/components/Header";
+import { CharacterInfo } from "@/utils/types";
 import { api } from "@/utils/utils";
-
-interface CharacterInfo {
-    name: string,
-    role: string,
-    health: number,
-    age: number,
-    race: string,
-    languages: string,
-    affiliation: string,
-    previous: string,
-    defect: string,
-    ideas: string,
-    personality: string,
-    history: string,
-    perception: number,
-    displacement: number,
-    initiative: number,
-    inspiration: number,
-    xp: number,
-    gold: number,
-    characterAttributes: [{
-        id: string,
-        attributesId: number,
-        value: number
-    }],
-    characterExpertise: [{
-        id: string,
-        expertiseId: number,
-        value: number
-    }],
-    characterItens: [{
-        id: string,
-        itemsId: number,
-        value: string,
-        bonusDamage: string,
-        typeDamage: string
-    }],
-    characterSkills: [{
-        id: string,
-        skillsId: number,
-        value: string,
-        bonusDamage: string,
-        typeDamage: string
-    }],
-    characterSpells: [{
-        id: string,
-        spellsId: number,
-        value: string,
-        bonusDamage: string,
-        typeDamage: string
-    }]
-
-}
 
 export default async function Home() {
     const id = 123123;
