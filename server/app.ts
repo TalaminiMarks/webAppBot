ef42bb2481e1c6d35abbcb65b8dfec1252e4bbed
TalaminiMarks <talamini2014@gmail.com>
2025-01-13 09:13:51 -0300

    fix name to cameCase

--- server/app.ts
+++ server/app.ts
@@ -2,7 +2,7 @@ import fastify from "fastify";
 import cors from "@fastify/cors"
 
 import character from "./src/routes/character";
-import create_character from "./src/routes/create-character";
+import createCharacter from "./src/routes/createCharacter";
 import attributes from "./src/routes/attributes";
 import expertise from "./src/routes/expertise";
 import equipament from "./src/routes/equipament";
@@ -10,7 +10,6 @@ import skills from "./src/routes/skills";
 import spells from "./src/routes/spells";
 import deleteItems from "./src/routes/deleteItems";
 
-// Raiz do servidor com o parâmetro de logger usando o pino-pretty
 const app = fastify({
     logger: {
         level: "info",
@@ -20,13 +19,13 @@ const app = fastify({
     }
 });
 
-// Registra as rotas na aplicação ou plugins. 
+// TODO: Need to fix origin to the correct URL
 app.register(cors, {
     origin: true,
     methods: ["GET", "POST", "DELETE", "PUT"]
 })
 app.register(character);
-app.register(create_character);
+app.register(createCharacter);
 app.register(attributes);
 app.register(expertise);
 app.register(equipament);
@@ -34,7 +33,7 @@ app.register(skills);
 app.register(spells);
 app.register(deleteItems);
 
-// Ouve uma porta para rodar o servidor
+
 app.listen({
     port: 3333
 }, (err)=>{
---
+++ server/src/routes/createCharacter.ts
@@ -0,0 +1,33 @@
+import { FastifyInstance } from "fastify";
+import z from "zod";
+
+import { prisma } from "../utils/prisma";
+
+// Rota para registrar um personagem
+export default async function route (fastify: FastifyInstance){
+    // Cria um zod schema para validação dos dados que serão recebidos
+    const schema = z.object({
+        id: z.string(),
+        name: z.string(),
+        role: z.string(),
+        initial: z.string()
+    });
+    // Declaração da rota
+    // fastify.post("/personagem/criar", async (req, res)=>{
+    //     // Recupera as info que foram passados na requisição e valida com o schema
+    //     const data = schema.parse(req.body);
+
+    //     // Cria um registro no DB
+    //     await prisma.character.create({
+    //         data: {
+    //             id: data.id,
+    //             name: data.name,
+    //             role: data.role,
+    //             initial: data.initial
+    //         }
+    //     })
+
+    //     // Retorna uma mensagem na requisição
+    //     res.send(JSON.stringify({ message: 'Deu certo' }))
+    // });
+}
