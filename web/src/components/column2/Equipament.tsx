3260fd76dc7b45a7d75365f161377c808ae76266
TalaminiMarks <talamini2014@gmail.com>
2025-01-14 08:07:11 -0300

    add select menu and options on modal to add item

--- web/src/components/column2/Equipament.tsx
+++ web/src/components/column2/Equipament.tsx
@@ -1,10 +1,10 @@
-import { axiosInstance } from "@/utils/utils"
-import { FieldProps, EquipamentProps, DataItemFieldProps } from "./types";
+import { api } from "@/utils/utils"
 import ItemField from "@/components/column2/equipamentComponents/ItemField";
 import AddItem from "./equipamentComponents/addItem";
+import { EquipamentsInfo, EquipamentsFieldProps, EquipamentProps, DataItemFieldProps } from "@/utils/types";
 
 
-function Field({name, value}: FieldProps){
+function Field({name, value}: EquipamentsFieldProps){
     return (
         <div className="w-full flex justify-between items-center pr-4">
             <span>{name}</span>
@@ -18,7 +18,9 @@ function Field({name, value}: FieldProps){
     )
 }
 
-export default function Equipament({gold, CharacterItens}: EquipamentProps){
+export default async function Equipament({gold, CharacterItens}: EquipamentProps){
+    const { data } = await api.get("/equipamentos");
+    const equipaments: EquipamentsInfo[] = data;
     return (
         <div className="w-full flex flex-col p-2 bg-purple-300 gap-2">
             <div className="w-full flex">
@@ -32,13 +34,21 @@ export default function Equipament({gold, CharacterItens}: EquipamentProps){
                 <div className="w-3/4 h-72 p-2 flex flex-col items-center gap-2 bg-slate-400 overflow-auto">
                     {
                         CharacterItens.map(async item => {
-                            const { data }: { data: DataItemFieldProps } = await axiosInstance.get(`/equipamentos/${item.itemsId}`);
+                            const { data }: { data: DataItemFieldProps } = await api.get(`/equipamentos/${item.itemsId}`);
                             return(
                                 <ItemField key={item.id} id={item.id} description={data.description} name={data.name}/>
                             )
                         })
                     }
-                    <AddItem />
+                    <AddItem>
+                        {
+                            equipaments.map(equipament => {
+                                return (
+                                    <option key={equipament.id} value={equipament.id}>{equipament.name}</option>
+                                )
+                            })
+                        }
+                    </AddItem>
                 </div>
             </div>
             <span className="w-full text-center uppercase">equipamento</span>
--- web/src/components/column2/equipamentComponents/addItem.tsx
+++ web/src/components/column2/equipamentComponents/addItem.tsx
@@ -1,12 +1,12 @@
 "use client"
 
 import { useRef } from "react";
-
 import { PlusIcon } from "@heroicons/react/24/solid";
 
-export default function AddItem(){
+
+export default function AddItem({children}: {children: React.ReactNode}){
     const modalRef = useRef<HTMLDivElement>(null);
-    const firstInputRef = useRef<HTMLInputElement>(null);
+    const firstInputRef = useRef<HTMLSelectElement>(null);
     
     function openModal(){
         if (modalRef.current !== null){
@@ -39,10 +39,18 @@ export default function AddItem(){
                 <PlusIcon />
             </button>
             <div ref={modalRef} className="fixed top-0 left-0 w-full h-full hidden bg-black bg-opacity-50 justify-center items-center z-50" onClick={handleClickOutside} onKeyDown={handleKeyDown} aria-modal="true">
-                <button className="absolute top-0 right-0 h-10 w-10 m-4 bg-black text-white hover:bg-white hover:text-black transition rounded" onClick={closeModal}>X</button>
-                <div className="w-1/2 h-1/2 bg-blue-300">
-                    <input type="text" ref={firstInputRef}/>
-                </div>
+                <button className="absolute top-1/4 right-1/4 my-2 mx-4 h-10 w-10 bg-black text-white hover:bg-white hover:text-black transition rounded" onClick={closeModal}>X</button>
+                <form className="w-1/2 h-1/2 p-4 flex flex-col items-center gap-4 bg-blue-300">
+                    <h2>Adicionar um item no inventário</h2>
+                    <select name="itens" id="itens" className="w-full rounded p-2" ref={firstInputRef} defaultValue="#">
+                        <option disabled value="#">Selecione um item</option>
+                        {children}
+                    </select>
+                    <div className="flex flex-col">
+                        <label htmlFor="damage">Dano da arma</label>
+                        <input type="text" name="damage" id="damage" />
+                    </div>
+                </form>
             </div>
         </>
     )
