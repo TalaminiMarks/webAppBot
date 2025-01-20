3f74511cdcf7e5091cb215439031131118db7c97
TalaminiMarks <talamini2014@gmail.com>
2025-01-20 15:01:42 -0300

    move route to equipaments file

--- server/src/routes/addItem.ts
+++
@@ -1,8 +0,0 @@
-import { FastifyInstance } from "fastify";
-
-export default async function route (fastify: FastifyInstance){
-    // TODO: fazer a rota de adicionar item
-    fastify.get("/adicionar-item", async (req, res)=>{
-        
-    });
-}
