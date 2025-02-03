92c2511b418ea7e12c578259066a365a549fa4a8
TalaminiMarks <talamini2014@gmail.com>
2025-02-03 20:49:16 -0300

    making changes on discord register

--- server/src/routes/discord.ts
+++ server/src/routes/discord.ts
@@ -7,56 +7,90 @@ import { prisma } from '../utils/prisma';
 // Pensar em como fazer um state
 const state = "algumacoisasecretaqueeunicopracadasessao"
 
+interface userResponseInfo {
+    id: string;
+    username: string;
+    avatar: string;
+    global_name: string;
+    email: string;
+}
+
 export default async function route(fastify: FastifyInstance){
-    fastify.get("/auth/discord/login", (req, res)=>{
-        const url = `https://discord.com/oauth2/authorize?client_id=1291018256571695155&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A3333%2Fauth%2Fdiscord%2Fcallback&scope=identify+guilds+guilds.members.read+email&state=${state}`
-
-        res.redirect(url);
-    })
-
-    fastify.get("/auth/discord/callback", async (req, res)=>{
-        const schema = z.object({
-            code: z.string(),
-            state: z.string()
+    fastify.post("/registrar/discord", async (req, res)=>{
+        const bodySchema = z.object({
+            code: z.string()
         })
 
-        const { code, state } = schema.parse(req.query)
+        const { code } = bodySchema.parse(req.body)
 
-        if (!code) throw new Error("Codigo não fornecido")
-        if (state !== state) throw new Error("Estado invalido")
+        // if (process.env.DISCORD_CLIENT_ID !== undefined && 
+        //     process.env.DISCORD_CLIENT_SECRET !== undefined && 
+        //     process.env.DIRCORD_REDIRECT_URL !== undefined &&
+        //     process.env.CLIENT_REDIRECT_URL !== undefined){
+        //     const params = new URLSearchParams({
+        //         client_id: process.env.DISCORD_CLIENT_ID,
+        //         client_secret: process.env.DISCORD_CLIENT_SECRET,
+        //         grant_type: "authorization_code",
+        //         code,
+        //         redirect_uri: process.env.DIRCORD_REDIRECT_URL
+        //     })
 
-        if (process.env.DISCORD_CLIENT_ID !== undefined && process.env.DISCORD_CLIENT_SECRET !== undefined && process.env.DIRCORD_REDIRECT_URL !== undefined){
-            const params = new URLSearchParams({
-                client_id: process.env.DISCORD_CLIENT_ID,
-                client_secret: process.env.DISCORD_CLIENT_SECRET,
-                grant_type: "authorization_code",
-                code,
-                redirect_uri: process.env.DIRCORD_REDIRECT_URL
-            })
-
-            const headers = {
-                'Content-Type': 'application/x-www-form-urlencoded',
-            }
+        //     const headers = {
+        //         'Content-Type': 'application/x-www-form-urlencoded',
+        //     }
     
-            const response = await axios.post(
-                "https://discord.com/api/oauth2/token", 
-                params, 
-                {
-                    headers
-                }
-            )
+        //     const response = await axios.post(
+        //         "https://discord.com/api/oauth2/token", 
+        //         params, 
+        //         {
+        //             headers
+        //         }
+        //     )
 
-            const userResponse = await axios.get(
-                "https://discord.com/api/users/@me",
-                {
-                    headers: {
-                        Authorization: `Bearer ${response.data.access_token}`,
-                        ...headers
-                    }
-                });
+        //     const userResponse = await axios.get(
+        //         "https://discord.com/api/users/@me",
+        //         {
+        //             headers: {
+        //                 Authorization: `Bearer ${response.data.access_token}`,
+        //             }
+        //         });
             
-            res.send(userResponse.data)
-        }
-        
+        //     const { id, username, avatar, global_name, email }: userResponseInfo = userResponse.data
+
+        //     const userExists = await prisma.user.findUnique({
+        //         where: {
+        //             discordId: id
+        //         }
+        //     })
+
+        //     if (!userExists) {
+        //         await prisma.user.create({
+        //             data: {
+        //                 discordId: id,
+        //                 avatar: avatar,
+        //                 globalName: global_name,
+        //                 username: username,
+        //                 email: email
+        //             }
+        //         })
+        //     }
+
+        //     const token = fastify.jwt.sign(
+        //     {
+        //         name: username
+        //     }, 
+        //     { 
+        //         sub: id,
+        //         expiresIn: '1d',
+        //     })
+
+        //     req.headers = {
+        //         cookie: token
+        //     }
+
+        //     console.log(req.headers.cookie)
+
+        //     res.redirect(process.env.CLIENT_REDIRECT_URL)
+        // }
     })
 }
