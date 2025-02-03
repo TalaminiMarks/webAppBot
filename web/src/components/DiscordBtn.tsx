0edcd8769eda8cfb6fd986e6b8cdec38f2ad2913
TalaminiMarks <talamini2014@gmail.com>
2025-02-03 20:43:31 -0300

    change url to log in

--- web/src/components/DiscordBtn.tsx
+++ web/src/components/DiscordBtn.tsx
@@ -1,12 +1,10 @@
-"use client"
-
 import Image from "next/image"
 import discIconWhite from "@/public/disc-icon-white.svg"
 
 export default function DiscordBtn(){
     return (
         <a 
-            href="http://localhost:3333/auth/discord/login"
+            href="http://localhost:3000/api/auth/discord/login"
             className="p-4 flex justify-center items-center gap-4 bg-disc rounded-xl hover:bg-[#414ff0] hover:scale-105 transition-all"
         >
             <Image src={discIconWhite} alt="Discord Icon" width={30} />
