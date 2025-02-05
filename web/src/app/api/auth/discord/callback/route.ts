2b62b2b70c2c38781d93d332e61ca10dc0f5548d
TalaminiMarks <talamini2014@gmail.com>
2025-02-05 14:10:49 -0300

    callback route from discord and set token as a cookie

--- web/src/app/api/auth/discord/callback/route.ts
+++ web/src/app/api/auth/discord/callback/route.ts
@@ -1,17 +1,25 @@
 import { api } from "@/utils/utils";
-import { NextRequest } from "next/server"
+import { NextRequest, NextResponse } from "next/server"
 
 export async function GET(req: NextRequest) {
     const { searchParams } = new URL(req.url)
 
     const code = searchParams.get("code");
+    const redirectTo = req.cookies.get("redirectTo")?.value
 
     if (!code) throw new Error("Codigo não provido");
 
     const registerResponse = await api.post("/registrar/discord", {
         code
     })
+    const { token } = registerResponse.data
+    const tokenExpiresInSeconds = 60 * 60
 
-    return new Response(registerResponse.data)
+    const redirectURL = redirectTo ?? new URL("/", req.url)
 
+    return NextResponse.redirect(redirectURL, {
+        headers: {
+            "set-cookie": `token=${token}; Path=/; HttpOnly; max-age=${tokenExpiresInSeconds}`
+        }
+    })
 }
