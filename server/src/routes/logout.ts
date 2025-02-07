18e5022622425d32996b470235538055bb49525c
TalaminiMarks <talamini2014@gmail.com>
2025-02-07 08:14:05 -0300

    add route to revoke discord token

---
+++ server/src/routes/logout.ts
@@ -0,0 +1,52 @@
+import { FastifyInstance } from "fastify";
+import { prisma } from "../utils/prisma";
+import z from "zod";
+import axios from "axios";
+
+export default function route(fastify: FastifyInstance) {
+    fastify.addHook('preHandler', async (req)=>{
+        await req.jwtVerify();
+    });
+
+    fastify.post("/logout/discord", async (req, res)=>{
+        const schema = z.object({
+            sub: z.string()
+        })
+
+        const jwt = schema.parse(req.user)
+
+        const user = await prisma.user.findUnique({
+            where: {
+                discordId: jwt.sub
+            },
+            select: {
+                accessToken: true
+            }
+        })
+
+        if(user){
+            const revocationCode = await axios.post("https://discord.com/api/oauth2/token/revoke",
+                {
+                    client_id: process.env.DISCORD_CLIENT_ID,
+                    client_secret: process.env.DISCORD_CLIENT_SECRET,
+                    token: user.accessToken,
+                    token_type_hint: "access_token"
+                },
+                {
+                    headers: {
+                        "Content-Type": "application/x-www-form-urlencoded"
+                    }
+                }
+            )
+            if(revocationCode.status === 200){
+                res.send({message: "Usuário desconectado"})
+            }
+            else {
+                res.send({message: "Algo deu errado"})
+            }
+        }
+        else{
+            res.send({message: "Usuário não encontrado"})
+        }
+    });
+}
