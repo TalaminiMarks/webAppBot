0392ee8b192bdab44e0595cc07bddc8f15aebb41
TalaminiMarks <talamini2014@gmail.com>
2025-02-07 08:27:11 -0300

    finish route to revoke code on discord API and reset the cookie

--- web/src/app/api/auth/discord/logout/route.ts
+++ web/src/app/api/auth/discord/logout/route.ts
@@ -2,22 +2,23 @@ import { NextRequest, NextResponse } from "next/server";
 import { api } from "@/utils/utils";
 
 export async function GET(req: NextRequest){
-    const url = new URL("/", req.url);
+    const url = new URL("/login", req.url);
 
     const token = req.cookies.get("token");
-    const response = await api.post("/logout/discord", {
-        "Authorization": `Bearer ${token}`
+    const response = await api.post("/logout/discord", null, {
+        headers: {
+            Authorization: `Bearer ${token?.value}`
+        },
     })
 
-    alert(response.data.message)
-    return NextResponse.redirect(url)
-
-    // if(response.status === 200){
-    //     req.cookies.delete("token");
-    //     alert(response.data.message)
-    //     return NextResponse.redirect(url)
-    // }
-    // else {
-    //     alert(response.data.message)
-    // }
+    if(response.status === 200){
+        return NextResponse.redirect(url, {
+            headers: {
+                'set-cookie': 'token=; Path=/; HttpOnly; max-age:0'
+            }
+        })
+    }
+    else {
+        throw new Error("Algo deu errado ao desconectar")
+    }
 }
