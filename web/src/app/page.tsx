3ff185c3867f252d3c0b9ed7746b16e8e43bed9d
TalaminiMarks <talamini2014@gmail.com>
2025-02-12 15:11:23 -0300

    add path to characters route

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -2,7 +2,8 @@ import Link from "next/link"
 
 export default async function Home() {
     return (
-        <div>
+        <div className="flex gap-2">
+            <Link href="/personagem">Personagem</Link>
             <Link href="/api/auth/discord/logout">Logout</Link>
         </div>
     )
