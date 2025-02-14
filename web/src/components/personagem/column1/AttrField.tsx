a6b2a9a8694d6c838e8be419770ff23a088d474d
TalaminiMarks <talamini2014@gmail.com>
2025-02-14 08:58:06 -0300

    remove inputs and add spans, will be updated later

--- web/src/components/personagem/column1/AttrField.tsx
+++ web/src/components/personagem/column1/AttrField.tsx
@@ -1,30 +1,20 @@
+import { getModAttr } from "@/utils/utils";
+
 interface AttrFieldProps {
     attribute: string;
-    value?: number;
-    proeficience?: number;
+    value: number;
 }
 
-export default function AttrField({ attribute, value, proeficience }: AttrFieldProps){
+export default function AttrField({ attribute, value }: AttrFieldProps){
+    const modAttr = getModAttr(value);
     return (
-        <div className="w-full p-2 flex justify-center items-center flex-col gap-1 bg-white">
-            <label htmlFor={attribute} className="text-lg font-bold uppercase">{attribute}</label>
+        <div className="w-32 p-2 flex justify-center items-center flex-col gap-1 bg-white">
+            <p className="text-lg font-bold uppercase">{attribute}</p>
             <div className="py-4 px-8 bg-stone-300 rounded">
-                <input 
-                    type="text" 
-                    name={attribute} 
-                    id={attribute} 
-                    defaultValue={value ? value : 0} 
-                    className="w-8 text-center text-xl"
-                />
+                <span>{value}</span>
             </div>
             <div className="px-2 rounded-xl border-2 border-slate-400">
-                <input 
-                    type="text"
-                    name={attribute + "proeficience"}
-                    id={attribute + "proeficience"}
-                    className="text-sm w-4 text-center" 
-                    defaultValue={proeficience ? proeficience : 0} 
-                />
+                <span>{modAttr > 0 ? "+" + modAttr : modAttr}</span>
             </div>
         </div>
     )
