63b0ba76622942cea7560983e2a9e59efdfff5bc
TalaminiMarks <talamini2014@gmail.com>
2025-02-14 15:42:40 -0300

    change native buttons to button component

--- web/src/components/personagem/HealthBar.tsx
+++ web/src/components/personagem/HealthBar.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { useState } from "react";
-import { MinusIcon, PlusIcon } from "@heroicons/react/16/solid";
+import { MinusBtn, PlusBtn } from "./geral/Buttons";
 
 interface HealthBarProps {
     currentHealth: number;
@@ -27,16 +27,12 @@ export default function HealthBar({ currentHealth, maxHealth }: HealthBarProps){
 
     return (
         <div className="py-2 px-4 w-full flex justify-between items-center gap-4 bg-purple-400">
-            <button onClick={loseHealth} className="w-10 h-6 rounded-md flex justify-center items-center bg-zinc-300">
-                <MinusIcon width={20} height={20} />
-            </button>
+            <MinusBtn onClick={loseHealth}/>
             <div className="relative bg-white w-full h-6 rounded-md">
                 <span className="absolute w-full h-4 text-center z-10 text-black">{health} / {maxHealth}</span>
                 <div className="absolute h-6 bg-red-500 rounded-md" style={{width: `${currentHealthPercent}%`}}></div>
             </div>
-            <button onClick={addHealth} className="w-10 h-6 rounded-md flex justify-center items-center bg-zinc-300">
-                <PlusIcon width={20} height={20} />
-            </button>
+            <PlusBtn onClick={addHealth} />
         </div>
     )
 }
