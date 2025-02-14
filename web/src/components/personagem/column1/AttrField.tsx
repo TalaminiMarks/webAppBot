e96c7afb81cac041dfd725c68937ebe709ac5a52
TalaminiMarks <talamini2014@gmail.com>
2025-02-14 15:41:16 -0300

    add buttons for interactive

--- web/src/components/personagem/column1/AttrField.tsx
+++ web/src/components/personagem/column1/AttrField.tsx
@@ -1,4 +1,8 @@
+"use client"
+
+import { useState, useEffect } from "react";
 import { getModAttr } from "@/utils/utils";
+import { MinusBtn, PlusBtn } from "../geral/Buttons";
 
 interface AttrFieldProps {
     attribute: string;
@@ -6,12 +10,32 @@ interface AttrFieldProps {
 }
 
 export default function AttrField({ attribute, value }: AttrFieldProps){
-    const modAttr = getModAttr(value);
+    const [attr, setAttr] = useState(value);
+    const [modAttr, setModAttr] = useState(getModAttr(value))
+
+    useEffect(()=>{
+        setModAttr(getModAttr(attr))
+    }, [attr])
+
+    function addPoint(){
+        setAttr(val => val + 1)
+    }
+
+    function removePoint(){
+        if(attr > value){
+            setAttr(val => val - 1)
+        }
+    }
+
     return (
-        <div className="w-32 p-2 flex justify-center items-center flex-col gap-1 bg-white">
-            <p className="text-lg font-bold uppercase">{attribute}</p>
-            <div className="py-4 px-8 bg-stone-300 rounded">
-                <span>{value}</span>
+        <div className="w-44 p-2 flex justify-center items-center flex-col gap-4 bg-white">
+            <div className="w-full">
+                <p className="text-center text-lg font-bold uppercase">{attribute}</p>
+            </div>
+            <div className="w-full flex justify-between items-center">
+                <MinusBtn onClick={removePoint} levelUp />
+                <span className="bg-stone-300 px-4 py-2">{attr}</span>
+                <PlusBtn onClick={addPoint} levelUp />
             </div>
             <div className="px-2 rounded-xl border-2 border-slate-400">
                 <span>{modAttr > 0 ? "+" + modAttr : modAttr}</span>
