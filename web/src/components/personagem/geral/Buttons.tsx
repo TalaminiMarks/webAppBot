0f205a262e03fff89e94e6d5a23f0ed3f30bf8e4
TalaminiMarks <talamini2014@gmail.com>
2025-02-15 13:33:01 -0300

    add indentation on button tag

--- web/src/components/personagem/geral/Buttons.tsx
+++ web/src/components/personagem/geral/Buttons.tsx
@@ -12,7 +12,11 @@ const animation = "hover:bg-black hover:text-white transition-colors"
 
 export function MinusBtn({ onClick, levelUp }: BtnProps){
     return (
-        <button onClick={onClick} disabled={levelUp ? false : true} className={`w-8 h-6 rounded-md flex justify-center items-center bg-zinc-300 ${animation}`}>
+        <button 
+            onClick={onClick} 
+            disabled={levelUp ? false : true} 
+            className={`w-8 h-6 rounded-md flex justify-center items-center bg-zinc-300 ${animation}`}
+        >
             <MinusIcon width={20} height={20} />
         </button>
     )
@@ -20,7 +24,11 @@ export function MinusBtn({ onClick, levelUp }: BtnProps){
 
 export function PlusBtn({ onClick, levelUp }: BtnProps){
     return (
-        <button onClick={onClick} disabled={levelUp ? false : true} className={`w-8 h-6 rounded-md flex justify-center items-center bg-zinc-300 ${animation}`}>
+        <button 
+            onClick={onClick} 
+            disabled={levelUp ? false : true} 
+            className={`w-8 h-6 rounded-md flex justify-center items-center bg-zinc-300 ${animation}`}
+        >
             <PlusIcon width={20} height={20} />
         </button>
     )
