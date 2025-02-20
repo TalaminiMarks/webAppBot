5f877e5f6d070152ada71da947def017fde23aa8
TalaminiMarks <talamini2014@gmail.com>
2025-02-20 10:02:32 -0300

    change file name and add getModAttr function

--- server/src/utils/utils.ts
+++ server/src/utils/utils.ts
@@ -3,3 +3,7 @@ import { PrismaClient } from "@prisma/client"
 export const prisma = new PrismaClient({
     log: ["error"]
 })
+
+export function getModAttr(value: number){
+    return Math.floor((value - 10) / 2);
+}
