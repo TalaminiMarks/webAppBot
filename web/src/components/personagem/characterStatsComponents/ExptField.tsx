230822238f9acdddac54e5934275d8d8548a6978
TalaminiMarks <talamini2014@gmail.com>
2025-02-21 08:55:49 -0300

    remove async on component

--- web/src/components/personagem/characterStatsComponents/ExptField.tsx
+++ web/src/components/personagem/characterStatsComponents/ExptField.tsx
@@ -4,7 +4,7 @@ interface ExptFieldProps {
     expertise: string;
 }
 
-export default async function ExptField({ id, value, expertise }: ExptFieldProps){
+export default function ExptField({ id, value, expertise }: ExptFieldProps){
     return (
         <div className="flex gap-2">
             <input type="radio" id={id} name={expertise} />
