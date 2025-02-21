fe852a5b15adc9c56b824b2d70d6d76f638d14dd
TalaminiMarks <talamini2014@gmail.com>
2025-02-21 08:55:14 -0300

    done functions to update attributes and expertises

--- web/src/components/personagem/CharacterStats.tsx
+++ web/src/components/personagem/CharacterStats.tsx
@@ -1,18 +1,19 @@
 "use client"
 
-import { useState, useRef, MouseEvent, KeyboardEvent } from "react";
+import { useState, useRef, MouseEvent, KeyboardEvent, useEffect } from "react";
 import { XMarkIcon } from "@heroicons/react/20/solid"
 
 import { api, getModAttr } from "@/utils/utils";
-import { characterAttributes, characterExpertise, DataInfo } from "@/utils/interfaces";
+import { characterAttributes, characterExpertise, Attribute, Expertise } from "@/utils/interfaces";
 import AttrField from "./characterStatsComponents/AttrField";
 import { MinusBtn, PlusBtn } from "./geral/Buttons";
+import ExptField from "./characterStatsComponents/ExptField";
 
 interface CharacterStatsProps {
     characterAttributes: characterAttributes[];
     characterExpertise: characterExpertise[];
-    attributes: DataInfo[];
-    expertise: DataInfo[];
+    attributes: Attribute[];
+    expertise: Expertise[];
 }
 
 export default function CharacterStats({ characterAttributes, characterExpertise, attributes, expertise }: CharacterStatsProps){
@@ -21,7 +22,7 @@ export default function CharacterStats({ characterAttributes, characterExpertise
 
     const [originalValue, setOriginalValue] = useState(0);
     const [characterAttributesList, setCharacterAttributesList] = useState(characterAttributes)
-    // const [characterExpertiseList, setCharacterExpertiseList] = useState(characterExpertise)
+    const [characterExpertiseList, setCharacterExpertiseList] = useState(characterExpertise)
 
     function checkFirstValue(target: characterAttributes, index: number){
         setOriginalValue(characterAttributes[index].value);
@@ -30,6 +31,21 @@ export default function CharacterStats({ characterAttributes, characterExpertise
         }
         else{
             return true;
+        }
+    }
+
+    function updateModAttribute(attribute: characterAttributes){
+        const expertiseFilter = expertise.filter(i => i.attributesId === attribute.attributesId)
+        for(let i = 0; i < expertiseFilter.length; i++){
+            const { id } = expertiseFilter[i]
+            const target = characterExpertiseList.find(i => i.expertiseId === id);
+            if (!target) throw new Error("Erro em atualizar o valor da pericia");
+            target.value = attribute.modValue;
+            setCharacterExpertiseList(prev => {
+                const filter = prev.filter(i => i.expertiseId !== id)
+                return [...filter, target]
+            })
+
         }
     }
 
@@ -44,6 +60,7 @@ export default function CharacterStats({ characterAttributes, characterExpertise
                         const filter = prev.filter(i => i.attributesId !== key)
                         return [...filter, target]
                     });
+                    updateModAttribute(target);
                 }
             }
         }            
@@ -54,7 +71,7 @@ export default function CharacterStats({ characterAttributes, characterExpertise
             if(characterAttributesList[i].attributesId === key){
                 const target = characterAttributesList[i];
                 if (checkFirstValue(target, i)){
-                    target.value -= 1;
+                    target.value += -1;
                     target.modValue = getModAttr(target.value);
                     setCharacterAttributesList(prev => {
                         const filter = prev.filter(i => i.attributesId !== key)
@@ -64,7 +81,6 @@ export default function CharacterStats({ characterAttributes, characterExpertise
             }
         }
     }
-    
 
     function openModal(){
         if(modalRef.current !== null){
@@ -101,11 +117,11 @@ export default function CharacterStats({ characterAttributes, characterExpertise
                 <div className="w-[95%] h-[95%] bg-red-400">
                     <button 
                         onClick={closeModal}
-                        className={`absolute top-6 right-12 h-10 w-10 flex justify-center items-center rounded bg-black text-white hover:bg-white hover:text-black transition`} 
+                        className="absolute top-6 right-12 h-10 w-10 flex justify-center items-center rounded bg-black text-white hover:bg-white hover:text-black transition"
                     >
                         <XMarkIcon width={32} height={32}/>
                     </button>
-                    <div className="w-full h-full px-12 flex justify-center items-center">
+                    <div className="w-full h-full px-12 flex justify-center items-center gap-16 ">
                         <div className="flex flex-col gap-2">
                             {
                                 attributes.map(attr => {
@@ -117,6 +133,17 @@ export default function CharacterStats({ characterAttributes, characterExpertise
                                             <AttrField attribute={attr.name} value={filter.value} modValue={filter.modValue}/>
                                             <PlusBtn onClick={() => addPoint(attr.id)} levelUp/>
                                         </div>
+                                    )
+                                })
+                            }
+                        </div>
+                        <div className="flex flex-col gap-2">
+                            {
+                                expertise.map(expertise => {
+                                    const filter = characterExpertiseList.find(i => i.expertiseId === expertise.id);
+                                    if(!filter) throw new Error("Erro nas pericias");
+                                    return (
+                                        <ExptField key={expertise.id} id={`${expertise.id}`} expertise={expertise.name} value={filter.value}/>
                                     )
                                 })
                             }
