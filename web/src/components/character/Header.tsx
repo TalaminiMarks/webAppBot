48718851f56fe8e58fe03e00cb17566320f3cbbc
TalaminiMarks <talamini2014@gmail.com>
2025-02-24 21:56:43 -0300

    remove duplicated data

--- web/src/components/character/Header.tsx
+++ web/src/components/character/Header.tsx
@@ -1,3 +1,5 @@
+import { capitalize } from "@/utils/utils";
+
 interface FieldProps {
     value?: string | number;
     description: string;
@@ -5,9 +7,9 @@ interface FieldProps {
 
 function Field({value, description}: FieldProps){
     return (
-        <div>
-            <span className="text-xl font-bold">{value}</span>
-            <p className="uppercase text-header-field">{description}</p>
+        <div className="flex gap-1">
+            <p className="uppercase">{description + ":"}</p>
+            <span className="font-bold">{value}</span>
         </div>
     )
 }
@@ -16,27 +18,22 @@ interface HeaderCharacterInfoProps {
     name: string;
     role: string;
     race: string;
-    previous: string;
     xp: number;
-    tendency: string;
-    userName: string;
+    level: number;
 }
 
-export default function Header({name, role, previous, race, userName, tendency, xp}: HeaderCharacterInfoProps){
+export default function Header({name, role, race, xp, level}: HeaderCharacterInfoProps){
     return (
-        <header className="py-6 flex justify-between items-center">
-            <section className="w-1/3 bg-red-300">
-                <Field value={name} description="nome do personagem"/>
-            </section>
-        
-            <section className="w-2/3 grid grid-cols-3 justify-center items-center bg-blue-200">
-                <Field description="classe e nível" value={role}/>
-                <Field description="Antecedente" value={previous}/>
-                <Field description="Nome do jogador" value={userName}/>
-                <Field description="Raça" value={race}/>
-                <Field description="Tendência" value={tendency}/>
-                <Field description="Pontos de Experiencia" value={xp}/>
-            </section>
+        <header className="py-4 flex justify-between items-center">
+            <div className="px-4">
+                <h2 className="text-3xl">{name.toUpperCase()}</h2>
+            </div>
+            <div className="w-2/3 flex justify-center items-center gap-4">
+                <Field description="Classe" value={capitalize(role)}/>
+                <Field description="Raça" value={capitalize(race)}/>
+                <Field description="Nivel" value={level}/>
+                <Field description="XP" value={xp}/>
+            </div>
         </header>
     )
 }
