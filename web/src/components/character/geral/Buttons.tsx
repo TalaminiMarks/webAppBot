3898cdbccbdd2bea51d41096a55aa3fdd6dc79b1
TalaminiMarks <talamini2014@gmail.com>
2025-02-25 18:53:44 -0300

    add close button

--- web/src/components/character/geral/Buttons.tsx
+++ web/src/components/character/geral/Buttons.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { MinusIcon, PlusIcon } from "@heroicons/react/20/solid"
+import { MinusIcon, PlusIcon, XMarkIcon } from "@heroicons/react/20/solid"
 import { MouseEventHandler } from "react"
 
 interface BtnProps {
@@ -30,6 +30,14 @@ export function PlusBtn({ onClick, levelUp }: BtnProps){
             className={`w-8 h-6 rounded-md flex justify-center items-center bg-zinc-300 ${animation}`}
         >
             <PlusIcon width={20} height={20} />
+        </button>
+    )
+}
+
+export function CloseBtn({ onClick }: BtnProps){
+    return (
+        <button className="absolute top-0 right-0 h-10 w-10 bg-black text-white hover:bg-white hover:text-black transition rounded" onClick={onClick}>
+            <XMarkIcon />
         </button>
     )
 }
