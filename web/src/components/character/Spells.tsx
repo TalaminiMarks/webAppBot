ab6b43be1795a7e31ea540ad8b010e039d76db8d
TalaminiMarks <talamini2014@gmail.com>
2025-02-27 19:52:02 -0300

    add form to update spell list

--- web/src/components/character/Spells.tsx
+++ web/src/components/character/Spells.tsx
@@ -1,20 +1,38 @@
 "use client"
 
-import { useRef, useState, KeyboardEvent, MouseEvent } from "react"
+import { useEffect, useRef, useState, KeyboardEvent, MouseEvent, FormEvent } from "react"
 import { CloseBtn } from "./geral/Buttons";
 import { characterSpells, SpellsTable } from "@/utils/interfaces";
 import Spell from "./spellsComponents/Spell";
+import { MinusIcon, PlusIcon } from "@heroicons/react/20/solid";
+import { api } from "@/utils/utils";
 
 interface SpellsProps {
     characterSpells: characterSpells[];
-    spells: SpellsTable[]
+    spells: SpellsTable[];
+    characterId: string;
 }
 
-export default function Spells({ characterSpells, spells }: SpellsProps){
-    const modalRef = useRef<HTMLDivElement>(null)
-    const focusInputRef = useRef<HTMLInputElement>(null)
+export default function Spells({ characterSpells, spells, characterId }: SpellsProps){
+    const modalRef = useRef<HTMLDivElement>(null);
+    const focusInputRef = useRef<HTMLInputElement>(null);
 
-    const [isOpenModal, setIsOpenModal] = useState(false)
+    const [isOpenModal, setIsOpenModal] = useState(false);
+    const [isOpenAddSpellWindow, setIsOpenAddSpellWindow] = useState(false);
+    const [characterSpellList, setCharacterSpellList] = useState(characterSpells);
+    const [newSpell, setNewSpell] = useState<characterSpells>({
+        id: "",
+        additionalDescription: "",
+        bonusDamage: "",
+        spellsId: 0,
+        typeBonusDamage: ""
+    })
+
+    useEffect(()=>{
+        if(newSpell.id !== ""){
+            setCharacterSpellList(prev => [...prev, newSpell])
+        }
+    }, [newSpell])
 
     function openModal(){
         setIsOpenModal(true);
@@ -22,6 +40,10 @@ export default function Spells({ characterSpells, spells }: SpellsProps){
 
     function closeModal(){
         setIsOpenModal(false);
+    }
+
+    function stateAddSpellWindow(){
+        setIsOpenAddSpellWindow(!isOpenAddSpellWindow);
     }
 
     function handleClickOutside(event: MouseEvent<HTMLDivElement>){
@@ -37,8 +59,26 @@ export default function Spells({ characterSpells, spells }: SpellsProps){
     }
 
     function handleMouseEnter(){
-        focusInputRef.current?.focus()
+        focusInputRef.current?.focus();
     }
+
+    function handleFormData(e: FormEvent<HTMLFormElement>){
+            e.preventDefault();
+            const formData = new FormData(e.currentTarget);
+            formData.append("characterId", characterId);
+            const data = Object.fromEntries(formData.entries());
+            console.log(data);
+            api.post("/magias/adicionar", data).then(response => {
+                if(response.status === 200){
+                    alert(response.data.message);
+                    setNewSpell(response.data.spell);
+                    setIsOpenAddSpellWindow(false);
+                }
+                else{
+                    alert("Erro ao adicionar item");
+                }
+            })
+        }
 
     return(
         <div>
@@ -50,11 +90,25 @@ export default function Spells({ characterSpells, spells }: SpellsProps){
                 onMouseEnter={handleMouseEnter}
             >
                 <input ref={focusInputRef} className="sr-only" />
-                <div className="relative w-1/3 h-[90%] py-12 px-4 bg-yellow-200 overflow-y-auto">
+                <div className="relative w-1/3 h-[90%] flex flex-col py-12 px-4 bg-yellow-200 overflow-y-auto">
                     <CloseBtn onClick={closeModal} />
-                    <div className="w-full h-full flex flex-col flex-wrap gap-2 bg-purple-300">
+                    <button className="relative w-full mb-2 shadow rounded-full bg-black text-white hover:bg-white hover:text-black transition" onClick={stateAddSpellWindow}>
+                        {isOpenAddSpellWindow ? 
+                        <div className="flex items-center justify-center gap-2">
+                            <MinusIcon width={35} height={35}/>
+                            <span>Fechar janela</span>
+                        </div> : 
+                        <div className="flex items-center justify-center gap-2">
+                            <PlusIcon width={35} height={35}/>
+                            <span>Adicionar magia</span>
+                        </div>}
+                    </button>
+
+                    
+
+                    <div className="relative w-full h-full flex flex-col flex-wrap gap-2 bg-purple-300">
                         {
-                            characterSpells.map(spell => {
+                            characterSpellList.map(spell => {
                                 const filter = spells.filter(i => i.id === spell.spellsId)
                                 return (
                                     <Spell 
@@ -71,6 +125,38 @@ export default function Spells({ characterSpells, spells }: SpellsProps){
                                 )
                             })
                         }
+
+                        <div className={`${isOpenAddSpellWindow ? "flex" : "hidden"} flex-col absolute w-full`}>
+                            <form className="w-full p-4 flex flex-col items-center gap-4 bg-blue-300" onSubmit={handleFormData}>
+                                <h2>Adicionar uma magia ao grimório</h2>
+                                <select name="spellId" id="spellId" className="w-full rounded p-2" defaultValue="#">
+                                    <option disabled value="#">Selecione uma magia...</option>
+                                    {
+                                        spells.map(spell => {
+                                            return (
+                                                <option key={spell.id} value={spell.id}>{spell.name}</option>
+                                            )
+                                        })
+                                    }
+                                </select>
+                                <div className="w-full flex justify-center items-center gap-4">
+                                    <div className="w-1/4 flex flex-col">
+                                        <label htmlFor="bonusDamage">Dano Bônus?</label>
+                                        <input type="text" name="bonusDamage" id="bonusDamage" />
+                                    </div>
+                                    <div className="w-1/4 flex flex-col">
+                                        <label htmlFor="bonusTypeDamage">Tipo do dano Bônus?</label>
+                                        <input type="text" name="bonusTypeDamage" id="typeBonusDamage" />
+                                    </div>
+                                    <div className="w-1/4 flex flex-col">
+                                        <label htmlFor="additionalDescription">Complementar descrição?</label>
+                                        <input type="text" name="additionalDescription" id="additionalDescription" />
+                                    </div>
+                                </div>
+                                
+                                <button type="submit" className="bg-zinc-400 p-4">Adicionar</button>
+                            </form>
+                        </div>
                     </div>
                 </div>
             </div>
