5ebfce7416ecc07de8a33ad6db635837f9c42ad1
TalaminiMarks <talamini2014@gmail.com>
2025-02-28 07:58:20 -0300

    add native capitaliza css property

--- web/src/components/character/Header.tsx
+++ web/src/components/character/Header.tsx
@@ -1,5 +1,3 @@
-import { capitalize } from "@/utils/utils";
-
 interface FieldProps {
     value?: string | number;
     description: string;
@@ -9,7 +7,7 @@ function Field({value, description}: FieldProps){
     return (
         <div className="flex gap-1">
             <p className="uppercase">{description + ":"}</p>
-            <span className="font-bold">{value}</span>
+            <span className="font-bold capitalize">{value}</span>
         </div>
     )
 }
@@ -29,8 +27,8 @@ export default function Header({name, role, race, xp, level}: HeaderCharacterInf
                 <h2 className="text-3xl">{name.toUpperCase()}</h2>
             </div>
             <div className="w-2/3 flex justify-center items-center gap-4">
-                <Field description="Classe" value={capitalize(role)}/>
-                <Field description="Raça" value={capitalize(race)}/>
+                <Field description="Classe" value={role}/>
+                <Field description="Raça" value={race}/>
                 <Field description="Nivel" value={level}/>
                 <Field description="XP" value={xp}/>
             </div>
