741d48136e6080ad885138fd77a0ad328155a4f9
TalaminiMarks <talamini2014@gmail.com>
2025-03-23 18:37:07 -0300

    add ref to component

--- web/src/components/character/geral/Buttons.tsx
+++ web/src/components/character/geral/Buttons.tsx
@@ -1,18 +1,20 @@
 "use client"
 
 import { MinusIcon, PlusIcon, XMarkIcon } from "@heroicons/react/20/solid"
-import { MouseEventHandler } from "react"
+import { LegacyRef, MouseEventHandler } from "react"
 
 interface BtnProps {
     onClick?: MouseEventHandler<HTMLButtonElement>;
     levelUp?: boolean;
+    ref?: LegacyRef<HTMLButtonElement>
 }
 
 const animation = "hover:bg-black hover:text-white transition-colors"
 
-export function MinusBtn({ onClick, levelUp }: BtnProps){
+export function MinusBtn({ onClick, levelUp, ref }: BtnProps){
     return (
         <button 
+            ref={ref}
             onClick={onClick} 
             disabled={levelUp ? false : true} 
             className={`w-8 h-6 rounded-md flex justify-center items-center bg-zinc-300 ${animation}`}
@@ -22,9 +24,10 @@ export function MinusBtn({ onClick, levelUp }: BtnProps){
     )
 }
 
-export function PlusBtn({ onClick, levelUp }: BtnProps){
+export function PlusBtn({ onClick, levelUp, ref }: BtnProps){
     return (
         <button 
+            ref={ref}
             onClick={onClick} 
             disabled={levelUp ? false : true} 
             className={`w-8 h-6 rounded-md flex justify-center items-center bg-zinc-300 ${animation}`}
@@ -34,9 +37,13 @@ export function PlusBtn({ onClick, levelUp }: BtnProps){
     )
 }
 
-export function CloseBtn({ onClick }: BtnProps){
+export function CloseBtn({ onClick, ref }: BtnProps){
     return (
-        <button className="absolute top-0 right-0 h-10 w-10 bg-black text-white hover:bg-white hover:text-black transition rounded" onClick={onClick}>
+        <button 
+            ref={ref} 
+            className="absolute top-0 right-0 h-10 w-10 bg-black text-white hover:bg-white hover:text-black transition rounded" 
+            onClick={onClick}
+        >
             <XMarkIcon />
         </button>
     )
