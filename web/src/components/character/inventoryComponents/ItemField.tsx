34c8dca671779a560d306a1958760eac82004e3f
TalaminiMarks <talamini2014@gmail.com>
2025-03-26 10:50:17 -0300

    move deleteRecord function to its parent

--- web/src/components/character/inventoryComponents/ItemField.tsx
+++ web/src/components/character/inventoryComponents/ItemField.tsx
@@ -1,7 +1,8 @@
 "use client"
 
-import { useRef, useState } from "react";
+import { useState, useEffect, MouseEventHandler } from "react";
 import { MinusIcon } from "@heroicons/react/24/solid";
+
 import { api } from "@/utils/utils";
 import { ItensTable } from "@/utils/interfaces";
 
@@ -9,15 +10,16 @@ interface ItemFieldProps extends ItensTable {
     bonusDamage?: string;
     typeBonusDamage?: string;
     additionalDescription?: string;
-    equipped: boolean
+    equipped: boolean;
+    handleDeleteRecord: MouseEventHandler<HTMLButtonElement>;
 }
 
 export default function ItemField({
-    id, name, description, damage, typeDamage, additionalDescription, bonusDamage, typeBonusDamage, equipped
+    id, name, description, damage, typeDamage, additionalDescription, bonusDamage, typeBonusDamage, equipped, weight, equippable, handleDeleteRecord
 }: ItemFieldProps){
     const [isMouseEnter, setIsMouseEnter] = useState(false);
-
-    const itemRef = useRef<HTMLDivElement>(null);
+    const [isEquipped, setIsEquipped] = useState(equipped);
+    const [isEquippedId, setIsEquippedId] = useState<string | number>();
 
     function handleMouseEnter(){
         setIsMouseEnter(true)
@@ -27,21 +29,37 @@ export default function ItemField({
         setIsMouseEnter(false)
     }
 
-    async function deleteRecord(){
-        if(itemRef.current !== null){
-            const { data } = await api.delete(`/equipamentos/deletar/${id}`);
-            alert(data.message);
-            itemRef.current.remove();
-        }
+    function handleEquipItem(){
+        setIsEquipped(!isEquipped)
+        setIsEquippedId(id)
     }
+
+    useEffect(()=>{
+        if(isEquippedId !== undefined){
+            const query = new URLSearchParams({
+                id: `${isEquippedId}`,
+                equipped: `${isEquipped}`
+            })
+            api.put(`/equipamentos/atualizar?${query}`)
+            .then(response => {
+                alert(response.data.message)
+            })
+        }
+    }, [isEquipped, isEquippedId])
+    
     return (
-        <div className="relative w-full p-2 flex justify-between items-center" ref={itemRef}>
-            <span onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>{name}</span>
+        <div className="relative w-full p-2 flex justify-between items-center">
+            <div className="flex items-center justify-center gap-4">
+                <span onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>{name}</span>
+                <div className="bg-red-400">
+                    <span className={isEquipped ? "block" : "hidden"}>Equipado!</span>
+                </div>
+            </div>
             <div className="flex items-center gap-10">
-                <button className="bg-blue-400 text-black w-32 h-8 rounded-3xl hover:bg-blue-700 hover:text-white transition-colors">
-                    {equipped ? "desequipar" : "equipar"}
+                <button className="bg-blue-400 text-black w-32 h-8 rounded-3xl hover:bg-blue-700 hover:text-white transition-colors" onClick={handleEquipItem}>
+                    {isEquipped ? "desequipar" : "equipar"}
                 </button>
-                <button className="w-6 h-6 rounded bg-black text-white hover:bg-white hover:text-black transition" onClick={deleteRecord}>
+                <button className="w-6 h-6 rounded bg-black text-white hover:bg-white hover:text-black transition" onClick={handleDeleteRecord}>
                     <MinusIcon />
                 </button>
             </div>
@@ -50,6 +68,8 @@ export default function ItemField({
                 <p className={`${additionalDescription ? "block" : "hidden"} mb-2`}>{additionalDescription}</p>
                 <p>Dano: {damage} - {typeDamage}</p>
                 <p className={`${bonusDamage && typeBonusDamage ? "block" : "hidden"}`}>Dano Bônus: {bonusDamage} - {typeBonusDamage}</p>
+                <p>Peso: {weight}</p>
+                <p className="font-bold">Equipavel? {equippable ? "Sim" : "Não"}</p>
             </div>
         </div>
     )
