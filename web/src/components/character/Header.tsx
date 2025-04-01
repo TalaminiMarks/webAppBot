398dc259a68122239efe9c588d15e5238b8d31fe
TalaminiMarks <talamini2014@gmail.com>
2025-04-01 20:15:17 -0300

    add home button

--- web/src/components/character/Header.tsx
+++ web/src/components/character/Header.tsx
@@ -1,3 +1,6 @@
+import Link from "next/link";
+import { HomeIcon } from "@heroicons/react/20/solid";
+
 interface FieldProps {
     value?: string | number;
     description: string;
@@ -26,12 +29,16 @@ export default function Header({name, role, race, xp, level}: HeaderCharacterInf
             <div className="px-4">
                 <h2 className="text-3xl">{name.toUpperCase()}</h2>
             </div>
-            <div className="w-2/3 flex justify-center items-center gap-4">
+            <div className="flex justify-center items-center gap-4">
                 <Field description="Classe" value={role}/>
                 <Field description="Raça" value={race}/>
                 <Field description="Nivel" value={level}/>
                 <Field description="XP" value={xp}/>
             </div>
+            <Link href={"/"} className="flex items-center px-4 py-2 mr-4 gap-1 rounded-xl bg-stone-400">
+                <HomeIcon width={24} height={24}/>
+                <span>Home</span>
+            </Link>
         </header>
     )
 }
