5856b763d008fd263522fcc919c2cc28fb4657fd
TalaminiMarks <talamini2014@gmail.com>
2025-04-02 21:05:18 -0300

    fix identation

--- server/src/routes/createCharacter.ts
+++ server/src/routes/createCharacter.ts
@@ -4,68 +4,68 @@ import { getModAttr, prisma } from "../utils/utils"
 
 export default function route(fastify: FastifyInstance){
     fastify.post("/personagem/criar", async (req, res)=> {
-            const schema = z.object({
-                id: z.string(),
-                name: z.string(),
-                age: z.string(),
-                baseRace: z.string(),
-                subRace: z.string(),
-                role: z.string(),
-                userId: z.string()
-            })
-    
-            const character = schema.parse(req.body)
-    
-            const user = await prisma.user.findUnique({
-                where: {
-                    discordId: character.userId
-                }
-            })
+        const schema = z.object({
+            id: z.string(),
+            name: z.string(),
+            age: z.string(),
+            baseRace: z.string(),
+            subRace: z.string(),
+            role: z.string(),
+            userId: z.string()
+        })
 
-            const attributes = await prisma.attributes.findMany();
-            const attributesData = [];
-            const baseValue = 8;
-            for(let i = 0; i < attributes.length; i++){
-                const obj = {attributesId: attributes[i].id, value: baseValue, modValue: getModAttr(baseValue)};
-                attributesData.push(obj);
+        const character = schema.parse(req.body)
+
+        const user = await prisma.user.findUnique({
+            where: {
+                discordId: character.userId
             }
+        })
 
-            const expertise = await prisma.expertise.findMany();
-            const expertiseData = [];
-            for(let i = 0; i < expertise.length; i++){
-                const obj = {expertiseId: expertise[i].id, value: getModAttr(baseValue)};
-                expertiseData.push(obj);
-            };
+        const attributes = await prisma.attributes.findMany();
+        const attributesData = [];
+        const baseValue = 8;
+        for(let i = 0; i < attributes.length; i++){
+            const obj = {attributesId: attributes[i].id, value: baseValue, modValue: getModAttr(baseValue)};
+            attributesData.push(obj);
+        }
 
-    
-            if(user){
-                await prisma.character.create({
-                    data: {
-                        id: character.id,
-                        name: character.name,
-                        age: character.age,
-                        role: character.role,
-                        baseRace: character.baseRace,
-                        subRace: character.subRace,
-                        userId: user.id,
-                        equipLoad: 30,
-                        characterAttributes: {
-                            createMany: {
-                                data: attributesData
-                            }
-                        },
-                        characterExpertise: {
-                            createMany: {
-                                data: expertiseData
-                            }
+        const expertise = await prisma.expertise.findMany();
+        const expertiseData = [];
+        for(let i = 0; i < expertise.length; i++){
+            const obj = {expertiseId: expertise[i].id, value: getModAttr(baseValue)};
+            expertiseData.push(obj);
+        };
+
+
+        if(user){
+            await prisma.character.create({
+                data: {
+                    id: character.id,
+                    name: character.name,
+                    age: character.age,
+                    role: character.role,
+                    baseRace: character.baseRace,
+                    subRace: character.subRace,
+                    userId: user.id,
+                    equipLoad: 30,
+                    characterAttributes: {
+                        createMany: {
+                            data: attributesData
+                        }
+                    },
+                    characterExpertise: {
+                        createMany: {
+                            data: expertiseData
                         }
                     }
-                })
-                res.send({ message: "Personagem criado com sucesso" })
-            }
-            else {
-                res.send({ message: "Erro: Usuario não cadastrado" })
-            }
-    
-        })
+                }
+            })
+            res.send({ message: "Personagem criado com sucesso" })
+        }
+        else {
+            res.send({ message: "Erro: Usuario não cadastrado" })
+        }
+
+    })
 }
